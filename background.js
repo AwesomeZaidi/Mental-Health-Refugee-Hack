5b4eed5cdb72af012109452a43c549b744e9f9fa
Rachel Woody <rachelwoody@Rachels-MacBook-Pro.local>
2019-01-12 16:56:30 -0800

    more personalized 4:56pm

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //Add instructions
+//The background script is the extension's event handler; it contains listeners for browser events that
+//are important to the extension. It lies dormant until an event is fired then performs the instructed logic.
+//An effective background script is only loaded when it is needed and unloaded when it goes idle.
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
